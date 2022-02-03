b2b91b482a01d740ad1a3b9f290565b6284defda
luisprofee <luisprofee@gmail.com>
2022-02-03 15:43:49 -0500

    prueba tecnica angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
-const routes: Routes = [];
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+
+const routes: Routes = [
+  
+  {path:'reactivo', component: ReactiveComponent},
+  {path:'usuarios', component: UsuariosComponent},
+  {path:'formulario', component: FormularioComponent},
+  {path: '**', pathMatch: 'full', redirectTo:'reactivo'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { UsersComponent } from './components/users/users.component';
+import { ComponentsModule } from './components/components.module';
+
+import { FormularioComponent } from './pages/formulario/formulario.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveComponent,
+    UsuariosComponent,
+    UsersComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
